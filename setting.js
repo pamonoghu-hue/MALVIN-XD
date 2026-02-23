c2c705eb12ce7a8aca925e9c562555a5774dd123
pamonoghu-hue <pamonoghu@gmail.com>
2026-02-23 07:45:22 +0100

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,9 +1,9 @@
 //=====🏅 ᴍᴀʟᴠɪɴ  xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'ᴍᴀʟᴠɪɴ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263714757857', // add your phone number
+  BOT_NAME: 'Storm xᴅ', // ur bot name
+  OWNER_NAME: 'frankbit', //your name
+  OWNER_NUMBER: '2348028192292', // add your phone number
   SESSION_ID: '', // add your session ID
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare' // ur timezone
